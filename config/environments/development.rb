71d031fe085b98eb678f97e2f13e5eb1648c3a56
maoleson <maoleson@pluto.dsu.edu>
2014-02-17 23:26:41 -0600

    'Validation!'

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ Depot::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  
+  # Turn on caching
+  config.action_controller.perform_caching = false
 end
